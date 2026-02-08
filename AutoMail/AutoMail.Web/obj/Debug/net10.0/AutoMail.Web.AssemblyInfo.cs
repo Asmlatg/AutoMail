96c80e09c3f5fd4ef097c7a583942ab0a805aec4
Asmlatg <asmaelatifi63@gmail.com>
2026-02-08 18:47:32 +0100

    Add Swagger for doc

--- AutoMail/AutoMail.Web/obj/Debug/net10.0/AutoMail.Web.AssemblyInfo.cs
+++ AutoMail/AutoMail.Web/obj/Debug/net10.0/AutoMail.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMail.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c348a2c46f9e323d873fe6453c7067b437956859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a0f235a9538b23fc22777dea8085d69680476f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMail.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMail.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
